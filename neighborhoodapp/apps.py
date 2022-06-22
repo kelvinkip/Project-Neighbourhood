5814031490082f36cc40cf789874b5038211f95f
kelvinkip <kelvinkipla@gmail.com>
2022-06-22 10:53:30 +0300

    app

--- neighborhoodapp/apps.py
+++ neighborhoodapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NeighborhoodappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'neighborhoodapp'
+
+    def ready(self):
+        import neighborhoodapp.signals
